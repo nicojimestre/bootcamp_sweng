647c508d63f29f227401f813bdfcf82731a15ca7
nicojimestre <njimenez5007@gmail.com>
2019-09-20 12:51:13 +0200

    rednu clone

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world ,");
+        System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
+    }
+    public static int computeUltimateAnswer() {
+	return 6 * 7;
     }
 }
